3876b9963b825fa1c5dc57c66657415a8df1851b
cl41re <muhammadrysdanial@gmail.com>
2026-04-18 19:38:16 +0800

    feat: added seo and new favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://danialazn.dev',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
